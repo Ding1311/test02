9d2429efd8b5200fd0ef1e85d83ccb0c62bb94ca
Ding1311 <1262079611@qq.com>
2020-02-28 16:48:52 +0800

    git添加vue-project项目

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<transition name="el-fade-in-linear">
+    <router-view />
+  </transition>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      loaded: true
+    }
+  }
+}
+</script>
+
+<style>
+  body,html {padding:0;margin:0;overflow: hidden;}
+</style>
---
+++ vue-project/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <!-- 头部区域 -->
+    <main-navbar></main-navbar>
+    <!-- 导航菜单栏 -->
+    <main-sidebar></main-sidebar>
+    <!-- 主内容区域 -->
+    <main-content></main-content>
+  </div>
+</template>
+
+<script>
+import MainNavbar from "./main/main-navbar";
+import MainSidebar from "./main/main-sidebar";
+import MainContent from "./main/main-content";
+
+export default {
+  name: "home",
+  components: {
+    MainNavbar,
+    MainSidebar,
+    MainContent
+  }
+};
+</script>
